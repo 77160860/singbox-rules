5719c8741842570adf13ef4c63434f194155434f
大 大 <77160860@qq.com>
2026-01-05 12:50:37 +0800

    Update main.go

--- sing-geoip/main.go
+++ sing-geoip/main.go
@@ -269,7 +269,18 @@ func release(source string, destination string, output string, ruleSetOutput str
 }
 
 func setActionOutput(name string, content string) {
-	os.Stdout.WriteString("::set-output name=" + name + "::" + content + "\n")
+	outputPath := os.Getenv("GITHUB_OUTPUT")
+	if outputPath == "" {
+		os.Stdout.WriteString(name + "=" + content + "\n")
+		return
+	}
+	f, err := os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, 0o644)
+	if err != nil {
+		os.Stdout.WriteString(name + "=" + content + "\n")
+		return
+	}
+	defer f.Close()
+	_, _ = f.WriteString(name + "=" + content + "\n")
 }
 
 func main() {

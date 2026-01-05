d70e15a279752340a5006be112c47779ffeeffb1
大 大 <77160860@qq.com>
2026-01-05 12:40:48 +0800

    Update main.go

--- sing-geosite/main.go
+++ sing-geosite/main.go
@@ -295,7 +295,7 @@ func release(source string, destination string, output string, cnOutput string, 
 func main() {
 	err := release(
 		"Loyalsoldier/v2ray-rules-dat",
-		"lyc8503/sing-geosite",
+		"77160860/sing-geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
